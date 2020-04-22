f5e7dc9f98b5562abd8d4773ac7975d6c6ad2822
sebastien <seb.millette@gmail.com>
2020-04-21 20:58:55 -0400

    fucked everything up

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+
 declare var $: any;
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"]
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
   popoverContent: string = "Content of the popover";
