39ef89d32389a5b36ab22160488ab861b872fece
Lucas <i.lucas.guillemette@gmail.com>
2020-05-23 11:02:20 -0400

    adding google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+import { filter } from "rxjs/operators";
+
+declare var gtag;
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
   title = "DemoForms";
 
-  lol() {
-    console.log("lol");
+  constructor(router: Router) {
+    const navEndEvents = router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    );
+    navEndEvents.subscribe((event: NavigationEnd) => {
+      gtag("config", "UA-167474754-1", {
+        page_path: event.urlAfterRedirects,
+      });
+    });
   }
 }
