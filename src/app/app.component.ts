48efd81e9413a297052f1ae4579361977c69153c
sebastien <seb.millette@gmail.com>
2020-06-05 13:38:19 -0400

    broke it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ declare var gtag;
 })
 export class AppComponent {
   title = "DemoForms";
-  loggedIn: boolean = true;
+  loggedIn: boolean = false;
   inputPassword: string = "";
 
   constructor(router: Router) {
